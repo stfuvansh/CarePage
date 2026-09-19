ebf79cb5e3903f366467189e1e55c3f6a4b2b45c
Vansh Srivastava <nyyxxxxxxz@gmail.com>
2026-09-19 20:23:00 +0530

    completed html and DOM

---
+++ frontend/app.js
@@ -0,0 +1,26 @@
+const patientNameInput = document.getElementById('patientNameInput');
+const dateInput = document.getElementById('dateInput');
+const searchBtn = document.getElementById('searchBtn');
+const imageUpload = document.getElementById('imageUpload');
+const recentPrescriptionsSection = document.getElementById('recentPrescriptionsSection');
+const recentPrescriptionsContainer = document.getElementById('recentPrescriptionsContainer');
+const searchResultsSection = document.getElementById('searchResultsSection');
+const searchResultsContainer = document.getElementById('searchResultsContainer');
+
+let selectedImage = null;
+imageUpload.addEventListener('change', function() {
+    const imageFile = imageUpload.files;
+    if (imageFile.length > 0) {
+        selectedImage = imageFile[0];
+         if (!selectedImage.type.startsWith('image/')) {
+            return;
+         }
+    }
+});
+
+const prescriptionData = {
+    patientName: patientNameInput.value,
+    prescriptionDate: dateInput.value,
+    image: selectedImage
+};
+
